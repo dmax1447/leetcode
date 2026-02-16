e8ff35ff76bc9db63eba9278628766ae4e8ce93c
Max Dragaytsev <mdragaytsev@synergy.ru>
2026-02-16 14:47:44 +0300

    upd

--- src/1472.design_browser_history.ts
+++ src/1472.design_browser_history.ts
@@ -19,43 +19,40 @@ class BrowserHistory {
 
   visit(url: string): void {
     const prev = this.state
-    this.state = new State({url, prev})
+    const newState = new State({url, prev})
+    this.state =  newState
+    prev.next = newState
   }
 
   back(steps: number): string {
-    console.log(`--- back start, ${steps}`)
-    console.log('before', this.state)
     while(steps > 0 && this.state.prev) {
       this.state = this.state.prev
       steps--
     }
-    console.log('after', this.state)
-    console.log('--- back end')
     return this.state.url
   }
 
   forward(steps: number): string {
-    console.log(`--- forward start, ${steps}`)
-    console.log('before', this.state)
     while(steps > 0 && this.state.next) {
       this.state = this.state.next
       steps--
     }
-    console.log('after', this.state)
-    console.log('forward end')
     return this.state.url
   }
 }
 
-
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * var obj = new BrowserHistory(homepage)
- * obj.visit(url)
- * var param_2 = obj.back(steps)
- * var param_3 = obj.forward(steps)
- */
-
 const browser = new BrowserHistory('leetcode.com')
 browser.visit('google.com')
+browser.visit('facebook.com')
+browser.visit('youtube.com')
+browser.back(1)
+browser.back(1)
+browser.forward(1)
+
+browser.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+browser.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+browser.back(2);
+
+browser.back(7);
+console.log(browser.state.url)
+// console.log(browser)
